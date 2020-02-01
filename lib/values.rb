8b82730e13fc0b61953fc5d61cc7aeba8601e60c
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-02-01 08:28:22 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -9,10 +9,7 @@ require 'pry'
 # }
 
 def get_the_values(groceries)
-  groceries = {
-    fruit: "Banana", 
-    vegetable: "Broccoli", 
-    dessert: "Cookie"
+  groceries.values.flatten
     
   }
 end
